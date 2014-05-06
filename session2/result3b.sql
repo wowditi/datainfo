c919fe8543de5ec7c53c65dbda38277799689162
wowditi <niels_geuze@hotmail.com>
2014-05-06 16:17:13 +0200

    3b

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,10 +1,9 @@
-﻿#1 Select DISTINCT p.pid 
-From Person p, Acts a
-WHERE EXISTS(
-	SELECT * FROM Acts a, Movie m
-	WHERE a.pid = p.pid
+﻿Select DISTINCT p.pid 
+From Person p, Acts a, Movie m
+WHERE a.pid = p.pid
 	AND  m.mid = a.mid
-		AND m.name = 'Back to the Future'
-);
+		AND m.name = 'Back to the Future';
 
-#1 shunting rule
+
+#1 shunting rule 
+#2 shunting rule 
