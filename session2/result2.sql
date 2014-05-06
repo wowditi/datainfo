df3db3d4450ea545f6bc22e0742382f85b064815
wowditi <niels_geuze@hotmail.com>
2014-05-06 15:47:40 +0200

    hey3

--- session2/result2.sql
+++ session2/result2.sql
@@ -1,4 +1,17 @@
-INSERT INTO "f
+INSERT INTO "f"
 VALUES('ABC', 'EF', false, 'ABC is een superkey'),
 ('E', 'D', true, 'E is geen key'),
 ( 'D', 'A', true, 'D is geen key');
+
+INSERT INTO "f1"
+VALUES('E', 'D', false, 'E is een superkey');
+
+INSERT INTO "f2"
+VALUES('ABC', 'EF', false, 'ABC is een superkey'),
+( 'E', 'A', true, 'E is geen key');
+
+INSERT INTO "f3"
+VALUES('E', 'A', false, 'E is een superkey');
+
+INSERT INTO "f4"
+VALUES('BC', 'EF', false, 'BC is een superkey');
