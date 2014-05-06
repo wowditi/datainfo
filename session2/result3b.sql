5ef91b63537267f901b1fab3da42366321ee1b5d
wowditi <niels_geuze@hotmail.com>
2014-05-06 16:33:12 +0200

    weershuntingrule

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,6 +1,5 @@
-﻿SELECT p.pid FROM Person p 
-WHERE EXISTS( 
-	SELECT * FROM Acts a 
-	WHERE a.pid = p.pid AND m.mid = a.mid 	AND m.name = 'Back to the Future' 
-	)	 
-); 
+﻿Select DISTINCT p.pid 
+From Person p, Acts a, Movie m
+WHERE a.pid = p.pid
+	AND  m.mid = a.mid
+		AND m.name = 'Back to the Future';
