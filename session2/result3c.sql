6e636fc8596a445e2d4dd596fb63944b38d3e937
wowditi <niels_geuze@hotmail.com>
2014-05-27 14:01:59 +0200

    1a

---
+++ session2/result3c.sql
@@ -0,0 +1,10 @@
+﻿SELECT p.name
+FROM Person p, Writes w
+WHERE w.pid = p.pid
+AND NOT EXISTS (
+SELECT *
+FROM Directs d
+WHERE d.mid = w.mid
+);
+		
+
---
+++ session2/result3d.sql
@@ -0,0 +1,11 @@
+﻿SELECT p.name
+FROM Person p, Writes w
+WHERE w.pid = p.pid
+AND NOT EXISTS (
+SELECT *
+FROM Directs d
+WHERE d.mid = w.mid
+) AND (SELECT COUNT(w.mid)
+FROM Writes w) <= 1;
+		
+
